8026ec07b159974c4807f2bd24017233aae3b9f5
Siddhant <84025779+methlox@users.noreply.github.com>
2023-02-09 00:17:11 +0530

    user database created req res created

--- src/customers/controllers/customers/customers.controller.ts
+++ src/customers/controllers/customers/customers.controller.ts
@@ -1,14 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe, Req, Res } from '@nestjs/common';
+import { MESSAGES } from '@nestjs/core/constants';
+import { Response } from 'express';
 import { CustomersService } from 'src/customers/services/customers/customers.service';
+import { Request } from 'supertest';
 
 @Controller('customers')
 export class CustomersController {
 
     constructor(private customersService: CustomersService){}
 
-    @Get('')
-    getCustomer() {
-        return this.customersService.findCustomer();
+    @Get(':id')
+    getCustomer(
+    @Param('id', ParseIntPipe) id: number,
+    @Req() req: Request,
+    @Res() res: Response
+    ) {
+        // console.log(typeof id);
+        const customer = this.customersService.findCustomerbyId(id);
+        if(customer){
+            res.send(customer);
+        } else{
+            res.status(400).send({msg: 'customer not found!'})
+        }
     }
 
 }
--- src/customers/services/customers/customers.service.ts
+++ src/customers/services/customers/customers.service.ts
@@ -2,13 +2,31 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class CustomersService {
-    findCustomer(){
-        return {
+
+    users = [
+        {
             id: 1,
             email: 'sid@gmail.com',
             createdAt: new Date(),
 
-        };
+        },
+        {
+            id: 2,
+            email: 'ryans@gmail.com',
+            createdAt: new Date(),
+
+        },
+        {
+            id: 3,
+            email: 'sans@gmail.com',
+            createdAt: new Date(),
+
+        }
+    ]
+
+    findCustomerbyId(id: number){
+        return this.users.find((user) => 
+        user.id === id)
     }
 
 }
